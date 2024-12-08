98eb1b88e31f62c37eed79ada667e078514fecd0
rayane-b <ray.hb.email@gmail.com>
2024-12-08 14:31:23 -0500

    burh

---
+++ load_data.py
@@ -0,0 +1,11 @@
+from datasets import load_dataset, DatasetDict
+
+# List of desired languages
+languages = ['en', 'fr', 'zh', 'ar', 'fa', 'sw', 'fi']
+
+# Load the dataset for each language
+for lang in languages:
+    dataset = load_dataset('unimelb-nlp/wikiann', lang)
+    print(lang)
+    # Access the dataset splits
+    dataset.save_to_disk(f"./processed_wikiann_{lang}")
